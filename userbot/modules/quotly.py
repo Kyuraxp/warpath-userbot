3e9964a6595fc5ef6dcfbfb71f6811403404e407
Kyura <96605016+Kyuraxp@users.noreply.github.com>
2022-02-27 12:08:31 +0700

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -9,13 +9,13 @@ from telethon.tl.functions.users import GetFullUserRequest
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import edit_delete, edit_or_reply, kyura_cmd
+from userbot.utils import edit_delete, edit_or_reply, skyzu_cmd
 from userbot.utils.misc import create_quotly
 
 from .carbon import all_col
 
 
-@kyura_cmd(pattern="q( (.*)|$)")
+@skyzu_cmd(pattern="q( (.*)|$)")
 async def quotly(event):
     match = event.pattern_match.group(1).strip()
     if not event.is_reply:
