bbbb8e3741e62c11cc7c45e0359cd37c4256e3f4
Kyura <96605016+Kyuraxp@users.noreply.github.com>
2022-02-28 04:44:04 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -38,7 +38,7 @@ async def help(event):
             "**⚡𝐊𝐘𝐔𝐑𝐀-𝐔𝐒𝐄𝐑𝐁𝐎𝐓​⚡**\n\n"
             f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
             "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-            f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n✐  support : @kyurasupport"
+            f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`\n✐  support : @kyurasupport2"
         )
         await asyncio.sleep(1000)
         await event.delete()
