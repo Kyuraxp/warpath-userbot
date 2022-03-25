52682b9cea19567b7e5204f17d6a28757f665e13
ҡʏᴜʀᴧ <96605016+Kyuraxp@users.noreply.github.com>
2022-03-25 11:00:25 +0700

    dhlh :)

--- userbot/modules/kyurhub.py
+++ userbot/modules/kyurhub.py
@@ -19,7 +19,7 @@ async def _(event):
         phubnya = [
             phub
             async for phub in event.client.iter_messages(
-                "@kyuraxphub2", filter=InputMessagesFilterVoice
+                "@kyuraxphub", filter=InputMessagesFilterVideo
             )
         ]
         aing = await event.client.get_me()
@@ -31,44 +31,6 @@ async def _(event):
         await event.delete()
     except Exception:
         await event.edit("Tidak bisa menemukan pornhub.com.")
-        
-@kyura_cmd(pattern="xxx$")
-async def _(event):
-    try:
-        xxxnya = [
-            xxx
-            async for xxx in event.client.iter_messages(
-                "@kyuraxphub3", filter=InputMessagesFilterPhotos
-            )
-        ]
-        aing = await event.client.get_me()
-        await event.client.send_file(
-            event.chat_id,
-            file=random.choice(xxxnya),
-            caption=f"Nih kak [{DEFAULTUSER}](tg://user?id={aing.id}) xxx nya",
-        )
-        await event.delete()
-    except Exception:
-        await event.edit("tidak bisa menemukan xxx.com.")
-
-@kyura_cmd(pattern="xnxx$")
-async def _(event):
-    try:
-        xnxxnya = [
-            xnxx
-            async for xnxx in event.client.iter_messages(
-                "@kyuraxphub", filter=InputMessagesFilterVideo
-            )
-        ]
-        aing = await event.client.get_me()
-        await event.client.send_file(
-            event.chat_id,
-            file=random.choice(xnxxnya),
-            caption=f"Nih kak [{DEFAULTUSER}](tg://user?id={aing.id}) xnxx nya",
-        )
-        await event.delete()
-    except Exception:
-        await event.edit("Tidak bisa menemukan xnxx.")
 
 
 CMD_HELP.update(
@@ -76,10 +38,6 @@ CMD_HELP.update(
         "phub": f"**Plugin : **`phub`\
         \n\n  •  **Syntax :** `{cmd}phub`\
         \n  •  **Function : **Untuk mengirim pornhub.com secara random.\
-        \n\n  •  **Syntax :** `{cmd}xxx`\
-        \n  •  **Function : **Untuk mengirim xxx.com secara random.\
-        \n\n  •  **Syntax :** `{cmd}xnxx`\
-        \n  •  **Function : **Untuk mengirim xnxx.com secara random.\
     "
     }
 )
